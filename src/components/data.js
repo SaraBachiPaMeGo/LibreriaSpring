e97f888f923101584053a2911c45cdc077592b2d
Sara Bachiller <sarabachiller8@gmail.com>
2020-03-27 22:24:11 +0100

    Spring

---
+++ src/components/data.js
@@ -0,0 +1,119 @@
+export default [
+    {
+        name: 'Rare Wind',
+        description: '#a8edea → #fed6e3',
+        css: 'linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)',
+        foto : "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg",
+        height: 200
+      },
+      { 
+        name: 'Saint Petersburg',
+        description: '#f5f7fa → #c3cfe2',
+        css: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)',
+        foto : "https://image.freepik.com/vector-gratis/divertidos-dibujos-animados-aguacate-surf_120675-89.jpg",
+  
+        height: 400
+      },
+      {
+        name: 'Deep Blue',
+        description: '#e0c3fc → #8ec5fc',
+        css: 'linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)',
+        foto : "https://image.freepik.com/vector-gratis/divertidos-dibujos-animados-pina-surf_120675-90.jpg",
+  
+        height: 400
+      },
+      {
+        name: 'Ripe Malinka',
+        description: '#f093fb → #f5576c',
+        css: 'linear-gradient(135deg, #f093fb 0%, #f5576c 100%)',
+        foto : "https://www.latercera.com/resizer/Wzxb6T4cv4VUAuaSbHoNvcDWypM=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/VUZA3KZTN5D2JN3SXNLS2D2QHQ.jpg",
+  
+        height: 400
+      },
+      {
+        name: 'Perfect White',
+        description: '#fdfbfb → #ebedee',
+        css: 'linear-gradient(135deg, #E3FDF5 0%, #FFE6FA 100%)',
+        foto : "https://data.whicdn.com/images/90165814/original.jpg",
+  
+        height: 400
+      },
+      {
+        name: 'Near Moon',
+        description: '#5ee7df → #b490ca',
+        css: 'linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)',
+        foto : "https://cflvdg.avoz.es/default/2018/04/04/00121522840703448616662/Foto/Mario.jpg",
+  
+        height: 400
+      },
+      {
+        name: 'Wild Apple',
+        description: '#d299c2 → #fef9d7',
+        css: 'linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)',
+        foto : "https://images3.wikia.nocookie.net/__cb20110717225240/lionking/images/thumb/e/e0/Cubsimba.png/270px-Cubsimba.png",
+  
+        height: 200
+      },
+      {
+        name: 'Ladoga Bottom',
+        description: '#ebc0fd → #d9ded8',
+        css: 'linear-gradient(135deg, #ebc0fd 0%, #d9ded8 100%)',
+        foto : "https://i.pinimg.com/originals/d3/f9/fd/d3f9fd164515d3412bfb1e88b43c66e4.jpg",
+  
+        height: 400
+      },
+      {
+        name: 'Sunny Morning',
+        description: '#f6d365 → #fda085',
+        css: 'linear-gradient(135deg, #f6d365 0%, #fda085 100%)',
+        foto : "https://i.pinimg.com/originals/e5/a6/5e/e5a65ebb1fe2c3afcb64e54bc7e84b46.jpg",
+  
+        height: 200
+      },
+      {
+        name: 'Lemon Gate',
+        description: '#96fbc4 → #f9f586',
+        css: 'linear-gradient(135deg, #96fbc4 0%, #f9f586 100%)',
+        foto : "https://ugc.kn3.net/i/760x/http://2.bp.blogspot.com/-Fp_-R0fe-Zo/TcdR0YIseCI/AAAAAAAAAAg/-TLjqZ3vuEg/s1600/bender-futurama.jpg",
+  
+        height: 400
+      },
+      {
+        name: 'Salt Mountain',
+        description: ' #FFFEFF → #D7FFFE',
+        css: 'linear-gradient(135deg, #FFFEFF 0%, #D7FFFE 100%)',
+        foto : " https://i.pinimg.com/originals/f4/8d/0e/f48d0efe3f767a053d7dc08541fa46c0.png",     
+        height: 200
+      },
+      {
+        name: 'New York',
+        description: ' #fff1eb → #ace0f9',
+        css: 'linear-gradient(135deg, #fff1eb 0%, #ace0f9 100%)',
+        foto:"https://i.pinimg.com/originals/c5/c5/a0/c5c5a008982cc00b6e2ab9a89aa14c56.png",
+        height: 400
+      },
+      {
+        name: 'Soft Grass',
+        description: ' #c1dfc4 → #deecdd',
+        css: 'linear-gradient(135deg, #c1dfc4 0%, #deecdd 100%)',
+        foto:"https://i.pinimg.com/originals/0e/37/79/0e37795ee8e704dc004dca922474bca6.png",
+  
+        height: 400
+      },
+      {
+        name: 'Japan Blush',
+        description: ' #ddd6f3 → #faaca8',
+        css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
+        foto:"https://miro.medium.com/max/1200/1*0y4J9IGKXpNRUInMmBNDmQ.png",
+        height: 200
+      },
+      {
+        name: 'Madrid Blush',
+        description: ' #ddd6f3 → #faaca8',
+        css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
+        foto:"https://i.pinimg.com/originals/44/14/37/4414372c1aa0c3e8032f49b3f64f2156.png",
+  
+        height: 200
+      }
+  ]
+  
